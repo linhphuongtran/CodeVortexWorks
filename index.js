071583e2358557829472fd4f9b900d8c0391bb44
linhphuongtran <samrdzmtz1pun0suy@hotmail.com>
2024-05-09 18:22:26 +0700

    test project commit

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-function maxDepth(root) {
-  if (!root) return 0;
-  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+function isValidBST(root) {
+  return isValid(root, null, null);
+}
+function isValid(node, min, max) {
+  if (!node) return true;
+  if ((min !== null && node.val <= min) || (max !== null && node.val >= max))
+    return false;
+  return (
+    isValid(node.left, min, node.val) && isValid(node.right, node.val, max)
+  );
 }
