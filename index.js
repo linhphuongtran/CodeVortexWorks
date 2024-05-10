3bf8c45f7e32d506096b9a66af9bae79112ee9dc
linhphuongtran <samrdzmtz1pun0suy@hotmail.com>
2024-05-10 22:20:57 +0700

    aptos project

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-function isValidBST(root) {
-  return isValid(root, null, null);
-}
-function isValid(node, min, max) {
-  if (!node) return true;
-  if ((min !== null && node.val <= min) || (max !== null && node.val >= max))
-    return false;
-  return (
-    isValid(node.left, min, node.val) && isValid(node.right, node.val, max)
-  );
-}
+console.log("hi");
